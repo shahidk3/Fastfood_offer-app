bba7fa264686b0c287dd609ed8d1c07f7856747f
Shahid Khan <140835291+shahidk3@users.noreply.github.com>
2024-03-01 12:03:31 +0530

    Update global_variables.dart

--- global_variables.dart
+++ global_variables.dart
@@ -2,16 +2,19 @@ final products = [
   {
     'title': 'All type of burgers',
     'price': 50,
+    'Company': 'xyz company',
     'imageurl': 'assets/images/burger.png',
   },
   {
     'title': 'cold drinks',
     'price': 40,
+    'Company': 'xyz company',
     'imageurl': 'assets/images/coke.png',
   },
   {
     'title': 'Different french fries',
     'price': 80,
+    'Company': 'xyz company',
     'imageurl': 'assets/images/fries.png',
   },
 
@@ -19,8 +22,10 @@ final products = [
 
 final cart = {
   {
-    'title': 'Different french fries',
+   'title': 'Different french fries',
     'price': 80,
-    'imageurl': 'assets/images/meal.png',
+    'Company': 'xyz company',
+    'imageurl': 'assets/images/fries.png',
+    
   },
 };
